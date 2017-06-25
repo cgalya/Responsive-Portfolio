b4e807c93f221ffbee64c1938e904c02e6f63e4a
Claire Galya <clairegalya@Claires-MacBook-Pro.local>
2017-06-25 13:33:44 -0400

    initial files

---
+++ javascript.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+	$('.lightbox_trigger').click(function(e) {
+		e.preventDefault();
+		var image_src = $(this).attr("src");
+		if ($('#lightbox').length > 0) {
+			$('#content').html('<img src="' + image_src + '" />');
+			$('#lightbox').show();
+		} else { 
+				var lightbox = 
+				'<div id="lightbox">' +
+				'<p>Click to close</p>' +
+				'<div id="content">' + 
+					'<img src="' + image_src +'" />' +
+				'</div>' +	
+				'</div>';
+				
+			$('body').append(lightbox);
+		}
+	});
+	$('#lightbox').live('click', function() { 
+		$('#lightbox').hide();
+	});
+});
